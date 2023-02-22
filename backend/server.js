61d2b1e7f155a2626d813057f156e8498e068c12
arijeet-Singh <104727895+arijeet-Singh@users.noreply.github.com>
2023-02-22 22:14:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "PUT", "DELETE");
+  res.header("Access-Control-Allow-Methods", "PUT");
   next();
 });
 app.use("/api", router);
